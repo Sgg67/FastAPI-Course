f6509743f451c43c9506eaa508e2306f7df0ba3f
sage <sage.yanoff@tcs.com>
2026-07-17 10:35:50 -0400

    add alembic

--- todo/alembic/env.py
+++ todo/alembic/env.py
@@ -1,8 +1,11 @@
 from logging.config import fileConfig
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-import models
+
 from alembic import context
+
+import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -10,8 +13,8 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
--- todo/database.py
+++ todo/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATBASE_URL = "mysql+pymysql://root:admin@127.0.0.1:3306/todo_app_db"
+SQLALCHEMY_DATBASE_URL = "sqlite:///./todos.db"
 
 # Create a sql alchemy engine
 engine = create_engine(SQLALCHEMY_DATBASE_URL)
